c8eb9368c1b11ad7a215bd3a3513197b933c2bf7
george <hyunrim03@naver.com>
2023-06-27 12:54:14 +0900

    feat: component 추가

--- fe/src/components/pages/Login.vue
+++ fe/src/components/pages/Login.vue
@@ -1,7 +1,13 @@
-<template></template>
+<template>
+  <div>
+    <layout-header></layout-header>
+  </div>
+</template>
 
 <script>
+import LayoutHeader from "../layout/LayoutHeader.vue";
 export default {
+  components: { LayoutHeader },
   name: "login-page",
 };
 </script>
